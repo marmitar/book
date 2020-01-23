5e5d06cc0b84104325cd629eb0c28e0d750dadf4
tiagodepalves <tiagodepalves@gmail.com>
2020-01-23 00:38:18 -0300

    Less restriction

--- cacher/src/lib.rs
+++ cacher/src/lib.rs
@@ -3,13 +3,38 @@
 use std::cell::Cell;
 
 
-pub struct Cacher<A, B, F: Fn(A) -> B>{
+pub struct Cacher<A, B, F>{
     calculation: F,
     value: Cell<Option<(A, B)>>
 }
 
-impl<A, B, F: Fn(A) -> B> From<F> for Cacher<A, B, F> {
-    fn from(calculation: F) -> Cacher<A, B, F> {
+impl<A: PartialEq + Copy, B: Copy, F> Cacher<A, B, F> {
+    fn get(&self, key: A) -> Option<B> {
+        self.value.get()
+            .and_then(|(k, v)| {
+                if k == key {
+                    Some(v)
+                } else {
+                    None
+                }
+            })
+    }
+
+    fn set(&self, key: A, value: B) {
+        self.value.set(Some((key, value)))
+    }
+}
+
+impl<A: PartialEq + Copy, B: Copy, F: Fn(A) -> B> Cacher<A, B, F> {
+    pub fn cache(&self, arg: A) {
+        if self.get(arg).is_none() {
+            self.set(arg, (self.calculation)(arg))
+        }
+    }
+}
+
+impl<A, B, F> From<F> for Cacher<A, B, F> {
+    fn from(calculation: F) -> Self {
         Cacher {
             calculation,
             value: Cell::new(None),
@@ -19,28 +44,30 @@ impl<A, B, F: Fn(A) -> B> From<F> for Cacher<A, B, F> {
 
 impl<A: PartialEq + Copy, B: Copy, F: Fn(A) -> B> Fn<(A,)> for Cacher<A, B, F>{
     extern "rust-call" fn call(&self, (arg,): (A,)) -> B {
-        if let Some((key, val)) = self.value.get() {
-            if key == arg {
-                return val;
-            }
-        }
-
-        let v = (self.calculation)(arg);
-        self.value.set(Some((arg, v)));
-        v
+        self.get(arg).unwrap_or_else(|| {
+            let x = (self.calculation)(arg);
+            self.set(arg, x);
+            x
+        })
     }
 }
 
-impl<A: PartialEq + Copy, B: Copy, F: Fn(A) -> B> FnMut<(A,)> for Cacher<A, B, F>{
-    extern "rust-call" fn call_mut(&mut self, args: (A,)) -> B {
-        self.call(args)
+impl<A: PartialEq + Copy, B: Copy, F: FnMut(A) -> B> FnMut<(A,)> for Cacher<A, B, F>{
+    extern "rust-call" fn call_mut(&mut self, (arg,): (A,)) -> B {
+        self.get(arg).unwrap_or_else(|| {
+            let x = (self.calculation)(arg);
+            self.set(arg, x);
+            x
+        })
     }
 }
 
-impl<A: PartialEq + Copy, B: Copy, F: Fn(A) -> B> FnOnce<(A,)> for Cacher<A, B, F>{
+impl<A: PartialEq + Copy, B: Copy, F: FnOnce(A) -> B> FnOnce<(A,)> for Cacher<A, B, F>{
     type Output = B;
 
-    extern "rust-call" fn call_once(self, args: (A,)) -> B {
-        self.call(args)
+    extern "rust-call" fn call_once(self, (arg,): (A,)) -> B {
+        self.get(arg).unwrap_or_else(|| {
+            (self.calculation)(arg)
+        })
     }
 }
